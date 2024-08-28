f01d606cb73ecf18e03e5d26161d22029827e7d6
Adriana del Pilar Apaza Tintaya <adrianadlpilar@gmail.com>
2024-08-28 17:01:17 -0500

    rails yelp mvp

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,21 @@ class ReviewsController < ApplicationController
   def new
     # We need @restaurant in our `simple_form_for`
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = @restaurant.reviews.new
+    @review = Review.new
+  end
+
+  def create
+    # no view needed
+    @restaurant = Restaurant.find(params[:restaurant_id]) #obtine cual es el restaurant
+    @review = Review.new(review_params) #crea el formulario
+    @review.restaurant = @restaurant #acepta el review hacia el restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
